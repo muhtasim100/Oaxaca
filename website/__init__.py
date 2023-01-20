964ac78460c0d0972e17707da36b47b7f789a96b
zkac421 <zkac421@live.rhul.ac.uk>
2023-01-20 15:20:19 +0000

    Moved static files (images, fonts, css) to a separate static folder, and modified the html and css code to match this

--- website/__init__.py
+++ website/__init__.py
@@ -7,7 +7,7 @@ def create_app():
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/auth/')
+    app.register_blueprint(auth, url_prefix='/auth')
 
     return app
 
