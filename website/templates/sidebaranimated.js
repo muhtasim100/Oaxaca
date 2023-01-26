fe6503855ddb2d14d204fc2e353ef407ff8f4fed
zkac421 <zkac421@live.rhul.ac.uk>
2023-01-26 12:20:56 +0000

    Fixed sidebar responsiveness

--- website/templates/sidebaranimated.js
+++ website/templates/sidebaranimated.js
@@ -1,18 +1,26 @@
-var originalWidth;
+var originalSidebarWidth;
 
 window.onload = function () {
     var nav = document.getElementsByClassName("navbar")[0];
-    originalWidth = 50;
-    nav.style.width = originalWidth + "px";
+    originalSidebarWidth = 40;
+    nav.style.width = originalSidebarWidth + "px";
+    
+    var content = document.getElementsByClassName("content")[0];
+    content.style.marginLeft = nav.style.width;
 }
 
 function toggleSidebar() {
     var nav = document.getElementsByClassName("navbar")[0];
     if (nav.classList.contains("expand")) {
-        nav.style.width = originalWidth + "px";
+        nav.style.width = originalSidebarWidth + "px";
         nav.classList.remove("expand");
     } else {
         nav.style.width = "16rem";
         nav.classList.add("expand");
     }
+}
+
+function isScreenSmall() {
+    var displayWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    return displayWidth < 600;
 }
