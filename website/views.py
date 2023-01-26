5031b50852d62ccaf72fc83329ebde95cbb16ca4
zjac178 <ZJAC178@live.rhul.ac.uk>
2023-01-26 20:45:51 +0000

    sidebar bug fixed

--- website/views.py
+++ website/views.py
@@ -5,3 +5,7 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/table')
+def tables():
+    return render_template("tables.html")
