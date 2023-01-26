4a7e2bf76229b48ec7487fc41b1fc3f19d19d5b7
zkac421 <zkac421@live.rhul.ac.uk>
2023-01-26 11:56:02 +0000

    Fixed sidebar expanding animation

--- website/templates/sidebaranimated.js
+++ website/templates/sidebaranimated.js
@@ -1,8 +1,18 @@
+var originalWidth;
+
+window.onload = function () {
+    var nav = document.getElementsByClassName("navbar")[0];
+    originalWidth = 50;
+    nav.style.width = originalWidth + "px";
+}
+
 function toggleSidebar() {
     var nav = document.getElementsByClassName("navbar")[0];
     if (nav.classList.contains("expand")) {
+        nav.style.width = originalWidth + "px";
         nav.classList.remove("expand");
     } else {
+        nav.style.width = "16rem";
         nav.classList.add("expand");
     }
 }
