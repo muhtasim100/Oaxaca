4613df84256d0908dab280f184afd19028009609
zkac421 <zkac421@live.rhul.ac.uk>
2023-01-26 17:50:17 +0000

    Moved top bar with logo from home.html to base.html

--- website/static/js/base.js
+++ website/static/js/base.js
@@ -2,17 +2,23 @@ var originalSidebarWidth;
 
 window.onload = function () {
     var nav = document.getElementsByClassName("navbar")[0];
+    var content = document.getElementsByClassName("content")[0];
     originalSidebarWidth = 40;
     nav.style.width = originalSidebarWidth + "px";
+    content.style.marginLeft = originalSidebarWidth + "px";
 }
 
 function toggleSidebar() {
     var nav = document.getElementsByClassName("navbar")[0];
+    var content = document.getElementsByClassName("content")[0];
+
     if (nav.classList.contains("expand")) {
         nav.style.width = originalSidebarWidth + "px";
+        content.style.marginLeft = originalSidebarWidth + "px";
         nav.classList.remove("expand");
     } else {
         nav.style.width = "16rem";
+        content.style.marginLeft = "16rem";
         nav.classList.add("expand");
     }
 }
