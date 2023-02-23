7506006f02e8ed117f5c39e69dd03088153b20cb
zjac331 <fausto.merlino.2020@live.rhul.ac.uk>
2023-02-23 19:15:27 +0000

    Style of the menu fixed and completed

--- website/views.py
+++ website/views.py
@@ -16,9 +16,19 @@ def tables():
 def menu():
     ListAll = FoodItem.query.all()
 
-    if FoodItem.query.count() != 1:
+    if FoodItem.query.count() != 6:
         test1 = FoodItem(FoodName = "Tacos", Quantity = 1, UnitPrice = 8.99, ItemCategory = "Main Courses", GlutenFree = False, ContainsMeat = True, Vegan = False, Cals = 458)
+        test2 = FoodItem(FoodName = "Quesadillas", Quantity = 1, UnitPrice = 6.99, ItemCategory = "Main Courses", GlutenFree = True, ContainsMeat = False, Vegan = False, Cals = 378)
+        test3 = FoodItem(FoodName = "Fajita", Quantity = 1, UnitPrice = 6.99, ItemCategory = "Main Courses", GlutenFree = False, ContainsMeat = True, Vegan = False, Cals = 378)
+        test4 = FoodItem(FoodName = "Burrito", Quantity = 1, UnitPrice = 6.99, ItemCategory = "Main Courses", GlutenFree = False, ContainsMeat = False, Vegan = True, Cals = 350)
+        test5 = FoodItem(FoodName = "Pozole", Quantity = 1, UnitPrice = 6.99, ItemCategory = "Main Courses", GlutenFree = False, ContainsMeat = False, Vegan = False, Cals = 503)
+        test6 = FoodItem(FoodName = "Menudo", Quantity = 1, UnitPrice = 6.99, ItemCategory = "Main Courses", GlutenFree = False, ContainsMeat = False, Vegan = False, Cals = 500)
         db.session.add(test1)
+        db.session.add(test2)
+        db.session.add(test3)
+        db.session.add(test4)
+        db.session.add(test5)
+        db.session.add(test6)
         db.session.commit()
         
     return render_template("menu.html", res= ListAll)
