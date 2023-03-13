c4f82136a767e9faa0eb091f74b1287bc1311c53
zkac421 <zkac421@live.rhul.ac.uk>
2023-03-13 12:41:53 +0000

    Finished cleaning up table code

--- website/static/js/tables.js
+++ website/static/js/tables.js
@@ -1,4 +1,6 @@
 const dropdowns = document.querySelectorAll('.dropdown');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+const tableButtons = document.querySelectorAll('.action');
 
 // ----------------------------------------
 
@@ -47,4 +49,35 @@ dropdowns.forEach(dropdown => {
             option.classList.add('active');
         });
     });
+});
+
+dropdownMenu.addEventListener('click', function(event) {
+  
+  const selectedOption = event.target.innerText; // get the text of the clicked dropdown item
+
+  
+  tableButtons.forEach(function(button) { // loop through all the table buttons
+    
+    const colorDot = button.querySelector('.numcircle');
+    
+    if (selectedOption === "Show all") { // show the color dot element if "Show all" is selected
+      button.style.display = "block";
+    } 
+   
+    else if (selectedOption === "Occupied" && colorDot.classList.contains('red-dot')) {  // show the red dot element if "A" is selected
+      button.style.display = "block"; 
+    }
+    
+    else if (selectedOption === "Reserved for the next two hours" && colorDot.classList.contains('yellow-dot')) { // show the yellow dot element if "B" is selected
+      button.style.display = "block";
+    }
+    
+    else if (selectedOption === "Available" && colorDot.classList.contains('green-dot')) { // show the green dot element if "C" is selected
+      button.style.display = "block"; 
+    }
+  
+    else {
+      button.style.display = "none";
+    }
+  });
 });
