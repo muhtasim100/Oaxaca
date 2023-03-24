38b0c8f65f9d2f8fc671f4d7256dadecb81b5d97
zkac421 <zkac421@live.rhul.ac.uk>
2023-03-24 12:36:36 +0000

    On enter key press on login password field it triggers login button click instead of sign up

--- website/static/js/login.js
+++ website/static/js/login.js
@@ -6,3 +6,11 @@ function togglePassword() {
       x.type = "password";
     }
 }
+
+const passwordInput = document.getElementById("password");
+passwordInput.addEventListener("keypress", function(event) {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    document.querySelector("#login-btn").click();
+  }
+});
