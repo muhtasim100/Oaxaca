e61e63a4c0e66ae2c721e5df146bf6c2a5f0d96e
zkac421 <zkac421@live.rhul.ac.uk>
2023-03-24 03:55:03 +0000

    404 frontend done

--- website/views.py
+++ website/views.py
@@ -398,3 +398,7 @@ def mockup():
 def trello():
     return redirect("https://trello.com/b/OG2F31ch/sprint-5-13-03-2023")
 
+
+@views.route("/404")
+def page_404():
+    return render_template("404.html")
